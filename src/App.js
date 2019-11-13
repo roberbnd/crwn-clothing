50351f4a1791912985c30be2419c6b8e7e4a0753
bnd <banda1915@gmail.com>
2019-11-12 20:10:56 -0600

    integrade firebase and compleate google sig in

--- src/App.js
+++ src/App.js
@@ -21,6 +21,7 @@ class App extends Component {
 
   componentDidMount() {
     this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+      this.setState({currentUser: user})
       console.log(user)
     })
   }
@@ -32,7 +33,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Header />
+        <Header currentUser={this.state.currentUser}/>
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
