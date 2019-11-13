72b0a6e0d4dd9a4a01d98e930e9d7fc4ce92aa9c
bnd <banda1915@gmail.com>
2019-11-12 20:32:50 -0600

    setup firestore

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from './App';
+//
+// it('renders without crashing', () => {
+//   const div = document.createElement('div');
+//   ReactDOM.render(<App />, div);
+//   ReactDOM.unmountComponentAtNode(div);
+// });
+//
+import firebase from 'firebase/app'
+import 'firebase/firestore'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+firestore.collection('users').doc('mUWhTy9Kb7euZkC7n8Xb').collection('cartItems').doc('YcUtuIZpJqHA6lgndsMw')
+firestore.doc('/users/mUWhTy9Kb7euZkC7n8Xb/cartItems/YcUtuIZpJqHA6lgndsMw')
+firestore.collection('/users/mUWhTy9Kb7euZkC7n8Xb/cartItems')
